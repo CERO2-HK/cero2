df1079dcae6916a4b262bf0b9b9488bc367eaf55
Kai Chung Ting <>
2022-04-14 13:49:34 +0800

    map pin

--- lib/screens/map/map_view.dart
+++ lib/screens/map/map_view.dart
@@ -1,7 +1,12 @@
 import 'dart:async';
-
+import 'dart:math';
+import 'dart:typed_data';
+import 'dart:ui';
+import 'package:cero2/constants/colors.dart';
+import 'package:cero2/services/global_service.dart';
 import 'package:cero2/widgets/light_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper_plus/flutter_swiper_plus.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:stacked/stacked.dart';
 
@@ -14,18 +19,27 @@ class MapView extends StatefulWidget {
 }
 
 class _MapViewState extends State<MapView> {
-  Completer<GoogleMapController> _controller = Completer();
+  static const LatLng center = LatLng(22.302711, 114.177216);
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  late BitmapDescriptor customIcon;
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  GoogleMapController? controller;
+  MarkerId? selectedMarker;
+  int _markerIdCounter = 1;
+
+  void _onMapCreated(GoogleMapController controller) {
+    this.controller = controller;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,17 +50,61 @@ class _MapViewState extends State<MapView> {
         MapViewModel model,
         Widget? child,
       ) {
+        if (model.isBusy) {
+          return Container();
+        }
         return Scaffold(
-          appBar: LightAppBar(title: "Green Map"),
-          body: GoogleMap(
-            mapType: MapType.hybrid,
-            initialCameraPosition: _kGooglePlex,
-            onMapCreated: (GoogleMapController controller) async {
-              _controller.complete(controller);
-              controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
-            },
-          ),
-        );
+            appBar: LightAppBar(title: "Green Map"),
+            body: Stack(children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Expanded(
+                    child: GoogleMap(
+                      onMapCreated: _onMapCreated,
+                      initialCameraPosition: const CameraPosition(
+                        target: LatLng(22.302711, 114.177216),
+                        zoom: 11.0,
+                      ),
+                      markers: Set<Marker>.of(model.markers.values),
+                    ),
+                  ),
+                ],
+              ),
+              Visibility(
+                visible: model.selectedId != null,
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: SafeArea(
+                    child: Container(
+                        width: Global.screenWidth,
+                        height: Global.s(184),
+                        child: Swiper(
+                          loop: false,
+                          itemCount: model.markerInfos.length,
+                          controller: model.swiperController,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              width: Global.screenWidth - Global.s(48),
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: Global.s(24)),
+                              height: Global.s(160),
+                              decoration: BoxDecoration(
+                                color: AppColor.white,
+                                borderRadius:
+                                    BorderRadius.circular(Global.s(24)),
+                              ),
+                              child: Center(
+                                child: Text(model.markerInfos[index].title),
+                              ),
+                            );
+                          },
+                        )),
+                  ),
+                ),
+              ),
+            ]));
       },
     );
   }
--- lib/screens/map/map_viewmodel.dart
+++ lib/screens/map/map_viewmodel.dart
@@ -1,3 +1,55 @@
+import 'package:cero2/services/global_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper_plus/flutter_swiper_plus.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:stacked/stacked.dart';
 
-class MapViewModel extends BaseViewModel {}
+class MarkerInfo {
+  String id;
+  LatLng position;
+  String title;
+
+  MarkerInfo({required this.id, required this.position, required this.title});
+}
+
+class MapViewModel extends FutureViewModel {
+  late final BitmapDescriptor customIcon;
+  late Map<MarkerId, Marker> markers;
+  int? selectedId;
+  SwiperController swiperController = SwiperController();
+
+  List<MarkerInfo> markerInfos = [
+    MarkerInfo(
+      id: "green common",
+      position: LatLng(22.325218, 114.172172),
+      title: "Green Common",
+    ),
+    MarkerInfo(
+      id: "starbucks",
+      position: LatLng(22.304067943099323, 114.17233134689556),
+      title: "Starbucks",
+    ),
+  ];
+
+  @override
+  Future<void> futureToRun() async {
+    customIcon = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(Global.s(48), Global.s(48))),
+        'assets/images/map-pin.png');
+    markers = <MarkerId, Marker>{};
+    for (int i = 0; i < markerInfos.length; i++) {
+      MarkerInfo markerInfo = markerInfos[i];
+      markers[MarkerId(markerInfo.id)] = Marker(
+          markerId: MarkerId(markerInfo.id),
+          position: markerInfo.position,
+          icon: customIcon,
+          onTap: () {
+            print(i);
+            selectedId = i;
+            swiperController.move(i);
+            notifyListeners();
+          });
+    }
+    notifyListeners();
+  }
+}
